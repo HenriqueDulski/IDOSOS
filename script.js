9194f23563c16d3a26df108a77a030ba85d11380
HenriqueDulski <dulski.luiz@escola.pr.gov.br>
2026-06-25 12:13:32 -0300

    25/06

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// Seleciona os elementos principais
+// Seleciona os elementos principais da pagina
 const botaoContraste = document.getElementById('botao-contraste');
 const botaoFonte = document.getElementById('botao-fonte');
 const botaoAjuda = document.getElementById('botao-ajuda');
 const corpoPagina = document.body;
 
-// Alterna a classe de alto contraste
+// Alterna a classe de alto contraste ao clicar
 botaoContraste.addEventListener('click', function() {
     corpoPagina.classList.toggle('alto-contraste');
 });
 
-// Alterna a classe para mudar e aumentar a fonte
+// Alterna a classe para mudar e aumentar a fonte ao clicar
 botaoFonte.addEventListener('click', function() {
     corpoPagina.classList.toggle('fonte-alterada');
 });
 
-// Exibe mensagem de alerta no botao de ajuda
+// Exibe mensagem de alerta no botao de ajuda ao clicar
 botaoAjuda.addEventListener('click', function() {
     alert('Ligue para o numero 190 se achar que foi vitima de um golpe.');
 });
